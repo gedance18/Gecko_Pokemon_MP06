1e73097d8f15e7329f88c74e848e150b6c2f3aff
gedance18 <gedance18yt@gmail.com>
2022-12-14 00:03:45 +0100

    finalizado y enviado por correo el repositorio

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"OpenCSV"},{"p":"<Unnamed>","l":"PokemonsUrl"},{"p":"<Unnamed>","l":"ScrappingTables"}];updateSearchResults();
